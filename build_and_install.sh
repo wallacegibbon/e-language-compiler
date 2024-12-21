037c3769847d36b3660bfbbe27fa56ba6d9440c7
wallace <wallacegibbon@aliyun.com>
2024-12-21 22:08:41 +0800

    Trivial Changes: The build and install script got simplified. The `case` statement can be used only once.

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,17 +1,22 @@
 #! /bin/sh
 set -e
 
-OS_TYPE=$(uname -s)
+UNAME_S=$(uname -s)
 
-case $OS_TYPE in
-MINGW*|CYGWIN*) INSTALL_PATH=$HOME/AppData/Local/programs/e-language-compiler;;
-*             ) INSTALL_PATH=/usr/local/bin;;
+case $UNAME_S in
+MINGW*|CYGWIN*) OS_TYPE=windows;;
+*             ) OS_TYPE=unix;;
 esac
 
-case $OS_TYPE in
-MINGW*|CYGWIN*) SUDO="";;
-*             ) SUDO=sudo;;
-esac
+if test $OS_TYPE = windows
+then INSTALL_PATH=$HOME/AppData/Local/programs/e-language-compiler
+else INSTALL_PATH=/usr/local/bin
+fi
+
+if test $OS_TYPE = windows
+then SUDO=""
+else SUDO=sudo
+fi
 
 echo Building the escript file...
 rebar3 escriptize
